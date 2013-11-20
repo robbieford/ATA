2ef1d886467615a50d7ecf3442eab65aab4b2e59
Robbie Campbell <robbieford2000@msn.com>
2013-11-20 09:22:28 -0800

    I love tag

---
+++ AllenTheAgent.java
@@ -0,0 +1,37 @@
+/**
+ * Assignment 2 Submission - CPE 480 Fall 2013
+ * 
+ * Can we find the goald?
+ * 
+ * Tag Ashby & Robert Campbell
+ * tgashby & rfcampbe
+ */
+
+package Agents.tgashby; // This is not a TODO: rename me to your Cal Poly username! :)
+
+import BotEnvironment.SearchBot.*;
+
+public class AllenTheAgent extends WumpusAgent
+{
+    
+    
+    public AllenTheAgent()
+    {
+        super();
+        setDeveloperName("Allen the Agent");
+    }
+
+    public void step()
+    {
+        //Check percepts
+        
+        //Set beliefs
+        
+        //
+    }
+
+    public void reset()
+    {
+        super.reset();
+    }
+}
